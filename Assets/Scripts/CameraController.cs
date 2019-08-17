a4a4410d95ee14edb4a60b2dc424da7963a7d516
Benjamin Gillott <bg357@cornell.edu>
2019-08-17 11:47:14 -0400

    Init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	public Transform player;
+    public Rigidbody rb;
+    public float smoothness = .125f;
+	public Vector3 offset;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     	// offset = transform.position - player.transform.position; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desPos = player.position + offset;
+        Vector3 smoothPos = Vector3.Lerp (transform.position, desPos, smoothness);
+
+        transform.position = smoothPos;
+
+        // transform.forward = rb.velocity;
+        // transform.LookAt(player);
+        //transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speedMod;	
+	public Text countText;
+	public Text winText;
+	public bool isGrounded;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+
+     }
+
+
+    void Update(){
+    	if (Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if(transform.position.y < -20){
+        	rb.velocity = Vector3.zero;
+    		rb.angularVelocity = Vector3.zero; 
+        	transform.position = new Vector3 (0, 3, 0);
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*speedMod);
+
+        //rb.AddRelativeForce(Vector3.forward * speedMod);
+
+    }
+
+
+
+
+    void OnTriggerEnter(Collider other){
+    	if(other.gameObject.CompareTag("Pick Up")){
+    		other.gameObject.SetActive(false);
+    		count += 1;
+    		SetCountText();
+    	}
+    }
+
+    void SetCountText(){
+    	countText.text = "Count: "+ count.ToString();
+
+    	if(count >= 8){
+    		winText.text = "おめでとう！";
+    	}
+    }
+}
